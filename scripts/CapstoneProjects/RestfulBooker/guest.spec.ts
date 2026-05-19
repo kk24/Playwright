e333e7938cfa8776de9d179aa4a25341bab4bd83
KKhandekar <koustubh.khandekar@gmail.com>
2026-05-19 13:12:34 +1200

    19/05 - Update

---
+++ scripts/CapstoneProjects/RestfulBooker/guest.spec.ts
@@ -0,0 +1,16 @@
+  // ------------------------------------------------
+    // Guest User – Positive Scenarios
+  // ------------------------------------------------
+
+import {restfulBookerE2ETest, expect, Page} from '../../../fixtures'; // Importing the custom fixtures defined in the fixtures file
+
+
+restfulBookerE2ETest.describe('Restful Booker - Guest User - Positive Scenarios', () => {
+
+
+
+
+    
+
+
+}); // End of test suite
